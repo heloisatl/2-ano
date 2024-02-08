66b2eece8f7fd8d95775a5c97d53f0a0a937ee45
Heloisa T. L <134729931+heloloisa@users.noreply.github.com>
2024-02-08 15:21:00 -0300

    Add files via upload

---
+++ phpHelo/if.php
@@ -0,0 +1,15 @@
+<?php
+
+$num = 10;
+
+if($num == 10)
+    echo "verdadeiro \n"; 
+else if($num > 10)
+    echo "maior\n";
+else 
+    echo "false\n";
+
+
+/** as chaves depois do if sao opcionais quando se tem apenas uma linha de codigo, mas obrigatorias quando tem duas ou mais */
+
+
