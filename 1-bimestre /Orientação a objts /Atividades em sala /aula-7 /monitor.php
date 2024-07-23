c612075a27c4cdf17f4af1b5667b78f9113aac8b
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2024-07-23 15:45:35 -0300

    Update and rename a to monitor.php

---
+++ 1-bimestre /Orientação a objts /Atividades em sala /aula-7 /monitor.php
@@ -0,0 +1,42 @@
+<?php
+
+class Monitor {
+
+    //Atributos
+    public $cor;
+    public $resolucao;
+    public $marca;
+
+    //Métodos
+    function ligar(){
+        echo "Computador ligado!\n";
+    }
+    function desligar(){
+        echo "Computador desligado!\n";
+    }
+    function mostrarImagem(){
+        echo "Computador mostrando imagem com a resolução ". $this->resolucao . " XX!\n";
+    }
+}
+/*function ligar(){
+    echo "Computador ligado!";
+}
+----Dentro de uma classe uma função vira MÉTODOS, fora da classe ela é FUNÇÃO---*/
+
+//Programa principal
+$monitor1 = new Monitor();
+$monitor1 ->cor = "Preto";
+$monitor1 ->resolucao ="1080x1920";
+$monitor1 ->marca = "AOC";
+$monitor1 ->ligar();
+$monitor1 ->mostrarImagem();
+$monitor1 ->desligar();
+
+
+$monitor2 = new Monitor();
+$monitor2 ->cor = "Vermelho";
+$monitor2 ->resolucao ="1080x1920";
+$monitor2 ->marca = "AOC";
+$monitor2 ->ligar();
+$monitor2 ->mostrarImagem();
+$monitor2 ->desligar();
