a3077719047f4d6478108900cf4af0d250974942
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2024-10-27 12:17:25 -0300

    Update execucao.php

--- 3-Bimestre/Orientação a objts/17-outubro/ConcessionárioaEnergia/execucao.php
+++ 3-Bimestre/Orientação a objts/17-outubro/ConcessionárioaEnergia/execucao.php
@@ -1,22 +1,27 @@
 <?php
 
+require_once("modelo/Residencial.php");
 require_once("modelo/Comercial.php");
 require_once("modelo/Industrial.php");
-require_once("modelo/Residencial.php");
 
-echo("Escolha se você quer calcular o valor da energia Residencial, Comercial ou Industrial \n1-Residencial-1\n2-Comercial-2\n3-Industrial-3\n");
-$escolha = readline(" ");
+echo "Escolha o tipo de consumo para calcular o valor da energia:\n1 - Residencial\n2 - Comercial\n3 - Industrial\n";
+$escolha = readline("Digite a opção: ");
 
-if($escolha ==1){
-    $energia = new Residencial();
-
-}else if($escolha ==2){
-    $energia = new Comercial();
-
-}else if($escolha ==3){
-    $energia = new Industrial();
-    
-}
+switch ($escolha) {
+    case 1:
+        $energia = new Residencial();
+        break;
+    case 2:
+        $energia = new Comercial();
+        break;
+    case 3:
+        $energia = new Industrial();
+        break;
+    default:
+        echo "Opção inválida.\n";
+    }
 
 $energia->SetConsumo(readline("Qual o seu consumo em kWh?")) . "\n";
 $energia->getValorFatura();
+
+echo $energia->__toString() . "\n";
