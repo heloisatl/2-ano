2c346c7a7e34c543ba79faf99eb2c5f187a44269
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2024-11-14 15:45:12 -0300

    Create execucao.php

---
+++ 3-Bimestre/Orientação a objts/14-novembro/Cafeteria/execucao.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once("modelo/Bebida.php");
+require_once("modelo/Comida.php");
+require_once("modelo/IPedido.php");
+require_once("modelo/Pedido.php");
+require_once("modelo/Produto.php");
+
+$escolha = 0;
+        do {
+            "** -------- MENU ------------**  " . "\n";
+            " 1 -------- ADCIONAR ---------* " . "\n";
+            " 2 -------- CANCELAR ----------* " . "\n";
+            " 3 -------- LISTAR ------------* " . "\n";
+            " 0 -------- SAIR --------------* " . "\n";
+
+            $escolha = readline("De qual serviço você precisa? ");
+
+            switch ($escolha) {
+    
+                case 1:
+
+                }
+                
+            }while ($escola != 0);
+
+        
